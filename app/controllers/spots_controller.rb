aad377304b5bbe1bf97ad79dbce0032548dc37ce
honami727 <honami727mogul@gmail.com>
2024-06-22 12:36:45 +0000

    編集機能追加

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -18,6 +18,13 @@ class SpotsController < ApplicationController
   end
 
   def edit
+    @spot = Spot.find(params[:id])
+  end
+  
+  def update
+    spot = Spot.find(params[:id])
+    spot.update(spot_params)
+    redirect_to spot_path(spot.id)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'spots/new'
   get 'spots' => 'spots#index'
   get 'spots/:id' => 'spots#show', as: 'spot'
-  get 'spots/edit'
+  get 'spots/:id/edit' => 'spots#edit', as: 'edit_spot'
+  patch 'spots/:id' => 'spots#update', as: 'update_spot'
   post 'spots' => 'spots#create'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
