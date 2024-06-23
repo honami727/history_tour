dc05e582825af16afba94caaf861e92e0db4c9f8
honami727 <honami727mogul@gmail.com>
2024-06-23 10:22:11 +0000

    ユーザー編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :spots
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
